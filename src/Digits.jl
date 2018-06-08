622bae46a1bf6febe90cf31855667d5ca3669148
Giovanni Papini <gpapo95@gmail.com>
2018-06-08 17:36:20 +0200

    Generalize methods with `Integer`s instead of `Int`s

--- src/Digits.jl
+++ src/Digits.jl
@@ -13,7 +13,7 @@ export
   digitroot
 
 
-function undigit(l::Vector{Int}, base::Int = 10)
+function undigit(l::Vector{<:Integer}, base::Int = 10)
   acc = 0
   for (i, digit) in enumerate(l)
     acc += digit * base ^ (i-1)
@@ -25,11 +25,11 @@ undigit(n::Integer) = n
 
 reversedigits(n::Integer) = undigit(reverse!(digits(n)))
 
-reversedigits(l::Array{Int,1}) = reverse(l)
+reversedigits(l::Array{<:Integer,1}) = reverse(l)
 
-reversedigits!(l::Array{Int,1}) = reverse!(l)
+reversedigits!(l::Array{<:Integer,1}) = reverse!(l)
 
-function digithist(l::Array{Int,1})
+function digithist(l::Array{T,1}) where {T<:Integer}
   h = zeros(Int,10)
   for i in 1:length(l)
     d = abs(l[i])
@@ -48,7 +48,7 @@ function digithist(n::Integer)
   return h
 end
 
-function isanagram(a::Array{Int,1}, b::Array{Int,1})
+function isanagram(a::Array{<:Integer,1}, b::Array{<:Integer,1})
   if length(a) != length(b)
     return false
   end
@@ -62,11 +62,11 @@ function isanagram(a::Array{Int,1}, b::Array{Int,1})
 end
 
 isanagram(a::Integer, b::Integer) = isanagram(digits(a),digits(b))
-isanagram(a::Integer, b::Vector{Int}) = isanagram(digits(a),b)
-isanagram(a::Vector{Int}, b::Integer) = isanagram(a,digits(b))
+isanagram(a::Integer, b::Vector{<:Integer}) = isanagram(digits(a),b)
+isanagram(a::Vector{<:Integer}, b::Integer) = isanagram(a,digits(b))
 
 
-function ispalindrome(l::Array{Int,1})
+function ispalindrome(l::Array{<:Integer,1})
   m = div(length(l),2)
   for i = 1:m
     if l[i] != l[end-i+1]
@@ -78,7 +78,7 @@ end
 
 ispalindrome(n::Integer) = ispalindrome(digits(n))
 
-function contains(a::Array{Int,1},b::Array{Int,1})
+function contains(a::Array{<:Integer,1},b::Array{<:Integer,1})
   la = length(a)
   lb = length(b)
   if la > lb
@@ -99,11 +99,11 @@ function contains(a::Array{Int,1},b::Array{Int,1})
   return false
 end
 
-contains(a::Array{Int,1},b::Integer) = contains(a,digits(b))
+contains(a::Array{<:Integer,1},b::Integer) = contains(a,digits(b))
 contains(a::Integer,b::Array{Int,1}) = contains(digits(a),b)
 contains(a::Integer,b::Integer) = contains(digits(a),digits(b))
 
-function endswith(a::Array{Int,1},b::Array{Int,1})
+function endswith(a::Array{<:Integer,1},b::Array{<:Integer,1})
   if a == b
     return true
   end
@@ -121,11 +121,11 @@ function endswith(a::Array{Int,1},b::Array{Int,1})
   return false
 end
 
-endswith(a::Array{Int,1},b::Integer) = endswith(a,digits(b))
-endswith(a::Integer,b::Array{Int,1}) = endswith(digits(a),b)
+endswith(a::Array{<:Integer,1},b::Integer) = endswith(a,digits(b))
+endswith(a::Integer,b::Array{<:Integer,1}) = endswith(digits(a),b)
 endswith(a::Integer,b::Integer) = endswith(digits(a),digits(b))
 
-function startswith(a::Array{Int,1},b::Array{Int,1})
+function startswith(a::Array{<:Integer,1},b::Array{<:Integer,1})
   if a == b
     return true
   end
@@ -143,11 +143,11 @@ function startswith(a::Array{Int,1},b::Array{Int,1})
   return false
 end
 
-startswith(a::Array{Int,1},b::Integer) = startswith(a,digits(b))
-startswith(a::Integer,b::Array{Int,1}) = startswith(digits(a),b)
+startswith(a::Array{<:Integer,1},b::Integer) = startswith(a,digits(b))
+startswith(a::Integer,b::Array{<:Integer,1}) = startswith(digits(a),b)
 startswith(a::Integer,b::Integer) = startswith(digits(a),digits(b))
 
-function crop(l::Array{Int,1},i::Int)
+function crop(l::Array{Int,1},i::Integer)
   if abs(i) > length(l)
     error("Crop value too big!")
   elseif abs(i) == length(l)
@@ -163,20 +163,20 @@ end
 
 crop(n::Integer,i::Int) = undigit(crop(digits(n),i))
 
-function replace!(l::Array{Int,1},idx::AbstractArray{Int,1},d::Array{Int,1})
+function replace!(l::Array{<:Integer,1},idx::AbstractArray{<:Integer,1},d::Array{<:Integer,1})
   l[idx] .= d
   return l
 end
 
-function replace!(l::Array{Int,1},old::Int,new::Int)
+function replace!(l::Array{<:Integer,1},old::Int,new::Int)
   l[l.==old] .= new
   return l
 end
 
-replace(l::Array{Int,1},idx::AbstractArray{Int,1},d::Array{Int,1}) = replace!(deepcopy(l),idx,d)
-replace(l::Array{Int,1},old::Int,new::Int) = replace!(deepcopy(l),old,new)
+replace(l::Array{<:Integer,1},idx::AbstractArray{<:Integer,1},d::Array{<:Integer,1}) = replace!(deepcopy(l),idx,d)
+replace(l::Array{<:Integer,1},old::Int,new::Int) = replace!(deepcopy(l),old,new)
 
-function replace(n::Integer,idx::AbstractArray{Int,1},d::Array{Int,1})
+function replace(n::Integer,idx::AbstractArray{<:Integer,1},d::Array{<:Integer,1})
   return undigit(reverse(replace(reverse(digits(n)),idx,d)))
 end
 
@@ -184,11 +184,11 @@ function replace(n::Integer,old::Int,new::Int)
   return undigit(replace(digits(n),old,new));
 end
 
-function select(l::Array{Int,1},idx::AbstractArray{Int,1})
+function select(l::Array{<:Integer,1},idx::AbstractArray{<:Integer,1})
   return l[idx]
 end
 
-function select(n::Integer,idx::AbstractArray{Int,1})
+function select(n::Integer,idx::AbstractArray{<:Integer,1})
   return undigit(reverse(reverse(digits(n))[idx]))
 end
 
@@ -200,11 +200,11 @@ function combine(a::Integer,b::Integer)
   return a*f+b
 end
 
-combine(a::Array{Int,1},b::Array{Int,1}) = [b;a]
-combine(a::Integer,b::Vector{Int}) = combine(a,undigit(b))
-combine(a::Vector{Int},b::Integer) = combine(a,digits(b))
+combine(a::Array{<:Integer,1},b::Array{<:Integer,1}) = [b;a]
+combine(a::Integer,b::Vector{<:Integer}) = combine(a,undigit(b))
+combine(a::Vector{<:Integer},b::Integer) = combine(a,digits(b))
 
-crosssum(l::Array{Int,1}) = sum(l)
+crosssum(l::Array{<:Integer,1}) = sum(l)
 crosssum(n::Integer) = sum(digits(n))
 
 function digitroot(n::Integer)
@@ -213,7 +213,7 @@ function digitroot(n::Integer)
   return copysign(r,n)
 end
 
-digitroot(l::Array{Int,1}) = digitroot(undigit(l))
+digitroot(l::Array{<:Integer,1}) = digitroot(undigit(l))
 
 
 end # module
